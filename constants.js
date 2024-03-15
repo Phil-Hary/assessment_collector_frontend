84b894b6a6c0e10f3c0d8bc191b827a6f15e825f
Philip Harrison A <harrisonharry01@gmail.com>
2024-03-15 12:31:35 +0530

    [ SETUP ] - initial commit

---
+++ constants.js
@@ -0,0 +1,11 @@
+const COMPONENT_TYPE_MAPPING = {
+    "text-field": "STRING",
+    "dropdown": "STRING",
+    "number-field": "INT",
+    "date-field": "DATE",
+    "text-area": "STRING"
+}
+
+export {
+    COMPONENT_TYPE_MAPPING
+}
